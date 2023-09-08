14a6519335bee912fe03748e62c3b2c216119cab
L7NEG <88356593+L7NEG@users.noreply.github.com>
2023-09-08 08:28:58 +0300

    V16 Changelogs
    Added Self > Equip Ballastic Armour Directly (Note You Have To Own The Ballastic Equipment What It Dose It Is Basiclly Equip The ballastic Armour Without Needing To Wait It To Be Dropped)
    
    Added Cool Weapon Mods In Weapons Menu (Credits: zz8761159_97610 On Discord)
    
    Added Back Rep Level 1000 in Self > LSCM Unlocker Menu (Credits: Silentsalo for the 1.67 Globals)
    
    Added All Request Services In Vehicles Tab (Credits: Me For Updating it)
    
    Added New Money Remover Instantly (Credits: Silentsalo)
    
    Added New Remote 300k/1s Nightclub (Now You Don't Need To Be In Nightclub's Safe Just Walk In The Night Club And The  active The Remote 300k/1s And Done (Credits: Silentsalo)
    
    Added New 15 Mil Section In Apartment Heists Section (Credits: Slon)
    
    Added Back All Removed Features From v15 From Self To Credits Section
    
    Added Back MW29 Unlock In Events Menu (Credits: ShinyWassabi for the 1.67 Globals From Globals Thread on UKC)
    
    Fixed Restore Deleted Vehicles Option
    
    Fixed All Grayed Out Toggles/Options Now They Shoould Be No More Grayed Out 
    
    Added Story Characters Money Editor Menu
    
    Updated Credits Section 
    
    And That's It Bro 
    
    I Hope You All To Have A Great Day In Your Real Life Enjoy ❤️

--- Kiddions/Automatic_Sell_Crate AFK_Male.lua
+++ Kiddions/Automatic_Sell_Crate AFK_Male.lua
@@ -1,9 +1,11 @@
 -- https://dsc.gg/l7neg-
 -- https://l7neg.tk
 --©️ 2023 L7NEG
+boolcl = false
+Cloop = false
 local WH = script("am_mp_warehouse")
-local function Cloop(bool)
-	while bool do
+local function Cloop()
+		while Cloop do
 		if WH:is_active() then
 			menu.send_key_up(83)
 			menu.send_key_press(69)
@@ -22,5 +24,3 @@ end, function()
 	Cloop(boolcl)
 	
 end)
-
-
